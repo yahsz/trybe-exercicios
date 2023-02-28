87ddf437370446fd15c624e4e5a34d075822aeaf
yahsz <yaskaravedovelli@hotmail.com>
2023-02-28 19:18:03 -0300

    primeiro exercicio JS

---
+++ fundamentos/introducao-a-javascript/dia-01-primeiros-passos/script.js
@@ -0,0 +1,19 @@
+let nota = 60;
+
+
+// se a nota for maior ou igual a 80
+if (nota >= 80) {
+       console.log("Parabens vc faz parte do nosso grupo de pessoas aprovadas!")
+}
+
+
+//se a nota for menor que 80 e igual ou maior que 60
+else if (nota < 80 && nota >= 60) {
+        console.log("Vc esta na nossa lista de espera!")
+}
+
+//se a nota for menor que 60
+if (nota < 60){
+        console.log("Infelizmente vc reprovou.")
+}
+
